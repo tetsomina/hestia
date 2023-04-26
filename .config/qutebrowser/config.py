fd257d389c44a42688aadbc34421bd0eb9a929b2
tetsomina <44659151+tetsomina@users.noreply.github.com>
2023-04-26 21:24:43 +0000

    Config change

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,28 +1,43 @@
 #!/usr/bin/env python3
 import alduin.draw
 
-# Load existing settings made via :set
-config.load_autoconfig()
+# Lets keep all configs here
+config.load_autoconfig(False)
 
 # everforest.draw.konda(c, {"spacing": {"vertical": 5, "horizontal": 8}})
 alduin.draw.konda(c)
 
+c.fonts.default_size = "12pt"
 c.fonts.default_family = "monospace"
-c.fonts.hints = "12pt monospace"
-c.fonts.keyhint = "12pt monospace"
-c.fonts.prompts = "12pt monospace"
-c.fonts.downloads = "12pt monospace"
-c.fonts.statusbar = "12pt monospace"
-c.fonts.contextmenu = "12pt monospace"
-c.fonts.messages.info = "12pt monospace"
-c.fonts.debug_console = "12pt monospace"
-c.fonts.completion.entry = "12pt monospace"
-c.fonts.completion.category = "12pt monospace"
+c.fonts.prompts = "default_size default_family" # Only one whose default is sans-serif
 c.url.start_pages = "~/.config/qutebrowser/startpage/index.html"
 c.url.default_page = "~/.config/qutebrowser/startpage/index.html"
-c.editor.command = ["/usr/bin/alacritty", "-e", "/usr/bin/vim {}"]
+c.editor.command = ["alacritty", "--class", "qute", "-e", "/usr/bin/nvim", "{}"]
 
+# External file selection
+c.fileselect.handler = "external"
+c.fileselect.folder.command = ["alacritty", "--class", "qute", "-e", "lf", "-selection-path", "{}"]
+c.fileselect.multiple_files.command = ["alacritty", "--class", "qute", "-e", "lf", "-selection-path", "{}"]
+c.fileselect.single_file.command = ["alacritty", "--class", "qute", "-e", "lf", "-selection-path", "{}"]
+
+# Dark mode bay-bee
 c.colors.webpage.darkmode.enabled =  False
+c.colors.webpage.preferred_color_scheme = "dark"
+
+# Settings
+c.auto_save.session = True
+c.content.blocking.method = "both"
+c.completion.open_categories = ["searchengines", "quickmarks", "bookmarks", "history", "filesystem"]
+c.downloads.remove_finished = 5000
+c.tabs.show = "multiple"
+c.tabs.favicons.show = "pinned"
+c.tabs.indicator.width = 2
+c.tabs.title.alignment = "center"
+c.tabs.padding = {"bottom":15, "left": 8, "right": 8, "top": 15}
+c.tabs.mousewheel_switching = False
+c.statusbar.padding = {"bottom": 6, "left": 6, "right": 6, "top": 6}
+c.hints.radius = 0
+c.keyhint.radius = 0
 
 # Tor browsing
 # c.content.proxy = 'socks5://localhost:9050/'
@@ -34,24 +49,24 @@ c.url.searchengines = {
     "!m": "https://www.google.com/maps/search/{}",
     "!r": "https://www.reddit.com/search?q={}",
 }
-# config.set("colors.webpage.darkmode.enabled", False)
-# Toggle darkmode css
-config.bind(
-    ",t",
-    'config-cycle content.user_stylesheets ~/.config/qutebrowser/alduin/alduin-all-sites.css ""',
-)
 
 # Qute-pass keubindings
 config.bind(
     ",pp",
-    "spawn --userscript qute-pass -d bemenu",
+    "spawn --userscript qute-pass",
+)
+
+config.bind(
+    ",pP",
+    "spawn --userscript qute-pass --password-only -d 'rofi -dmenu -p Qute-pass'",
 )
 
 config.bind(
     ",pu",
-    "spawn --userscript qute-pass --username-only -d bemenu",
+    "spawn --userscript qute-pass --username-only -d 'rofi -dmenu -p Qute-pass'",
 )
 
+# Open with mpv
 config.bind(
     ",m",
     "spawn mpv {url}",
@@ -62,19 +77,13 @@ config.bind(
     "hint links spawn mpv {hint-url}",
 )
 
+# Swap J/K tab movement
 config.bind(
-    ",pP",
-    "spawn --userscript qute-pass --password-only -d bemenu",
+    "J",
+    "tab-prev",
 )
 
-# Open download
 config.bind(
-    ",d",
-    "spawn --userscript open_download",
-)
-
-# Cycle inputs
-config.bind(
-    "gi",
-    "jseval -q -f ~/.config/qutebrowser/cycle-inputs.js",
+    "K",
+    "tab-next",
 )
